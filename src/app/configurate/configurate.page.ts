6f9b97e3ab740acc81093017ff9af0515a435e25
sebastian <2202049@utrivieramaya.edu.mx>
2026-02-08 16:09:09 -0500

    esta mas o menos decorado, faltan paginas

--- src/app/configurate/configurate.page.ts
+++ src/app/configurate/configurate.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList,IonItem,IonBackButton, IonAvatar, IonLabel, IonButtons, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { home, settings, logOut, lockClosed, notifications, language, headset, informationCircle } from 'ionicons/icons';
 
+addIcons({ home, settings, 'log-out': logOut,'lock-closed': lockClosed, notifications, language, headset, informationCircle }); // Registro explícito por si acaso
 @Component({
   selector: 'app-configurate',
   templateUrl: './configurate.page.html',
   styleUrls: ['./configurate.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonList,IonItem,IonBackButton,IonAvatar,IonLabel,IonButtons,IonIcon,IonButton]
 })
 export class ConfiguratePage implements OnInit {
 
   constructor() { }
+
+
 
   ngOnInit() {
   }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,17 +5,16 @@ import {
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { home, settings, logOut } from 'ionicons/icons';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular'; // 1. Importar MenuController
 
-addIcons({ home, settings, logOut });
+addIcons({ home, settings, 'log-out': logOut }); // Registro explícito por si acaso
 
 @Component({
   selector: 'app-menu',
-  //templateUrl: './menu.component.html',
   standalone: true,
-  styleUrls: ['./menu.component.scss'],
   imports: [IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel],
-  template: 
-  `
+  template: `
     <ion-menu contentId="main-content" type="overlay">
       <ion-header>
         <ion-toolbar color="primary">
@@ -24,27 +23,39 @@ addIcons({ home, settings, logOut });
       </ion-header>
       <ion-content>
         <ion-list>
-          <ion-item button>
+          <ion-item button (click)="navigate('/home')">
             <ion-icon slot="start" name="home"></ion-icon>
             <ion-label>Inicio</ion-label>
           </ion-item>
-          <ion-item button>
+          
+          <ion-item button (click)="navigate('/configurate')">
             <ion-icon slot="start" name="settings"></ion-icon>
             <ion-label>Configuración</ion-label>
           </ion-item>
-          <ion-item button>
-            <ion-icon slot="end" name="logOut"></ion-icon>
-            <ion-label>Cerrar sesión</ion-label>
+          
+          <ion-item button (click)="logout()">
+            <ion-icon slot="start" name="log-out"></ion-icon> <ion-label>Cerrar sesión</ion-label>
           </ion-item>
         </ion-list>
       </ion-content>
     </ion-menu>
   `
 })
-export class MenuComponent  implements OnInit {
+export class MenuComponent implements OnInit {
 
-  constructor() { }
+  // 2. Inyectar MenuController
+  constructor(private router: Router, private menuCtrl: MenuController) { }
+
+  async navigate(url: string) {
+    await this.menuCtrl.close(); // 3. Cerrar el menú antes de navegar
+    this.router.navigateByUrl(url);
+  }
+
+  async logout() {
+    await this.menuCtrl.close();
+    console.log('Cerrando sesión...');
+    // Aquí tu lógica de borrar token, etc.
+  }
 
   ngOnInit() {}
-
 }
